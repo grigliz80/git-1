2e682cb0982f919e5331626f14fbbbad2214e9b1
Grygoriy <grigoriylizanets@gmail.com>
2026-04-09 08:49:31 +0300

    feat: add dish to MenuDay

--- src/luna_weekly_cooking/models/menu_day.py
+++ src/luna_weekly_cooking/models/menu_day.py
@@ -2,3 +2,6 @@ class MenuDay:
     def __init__(self, day_name: str):
         self.day_name = day_name
         self.dishes = []
+
+    def add_dish(self, dish):
+        self.dishes.append(dish)
--- tests/test_menu_day.py
+++ tests/test_menu_day.py
@@ -1,7 +1,16 @@
-from luna_weekly_cooking.models import MenuDay
+from luna_weekly_cooking.models import MenuDay, Dish
 
 def test_menu_day_creation():
     day = MenuDay("Monday")
 
     assert day.day_name == "Monday"
     assert day.dishes == []
+
+def test_add_dish():
+    day = MenuDay("Monday")
+    dish = Dish("Borsch", "first")
+
+    day.add_dish(dish)
+
+    assert len(day.dishes) == 1
+    assert day.dishes[0] == dish
